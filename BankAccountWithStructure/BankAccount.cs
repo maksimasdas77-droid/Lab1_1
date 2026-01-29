bafff0ce3d1b27bc759e7b8607abefad389dad49
Максим Дмитрачёнок <maksimasdas77@gmail.com>
2026-01-29 22:34:22 +0300

    продолжаю делать лабу, обновляю данные

---
+++ BankAccountWithStructure/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountWithStructure
+{
+    internal struct BankAccount
+    {
+        public long accNo;
+        public decimal accBalance;
+        public AccountType accType;
+        public BankAccount(long accountNumber, decimal balance, AccountType type)
+        {
+            accNo = accountNumber;
+            accBalance = balance;
+            accType = type;
+        }
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine("=== Информация о банковском счете ===");
+            Console.WriteLine($"Номер счета: {accNo}");
+            Console.WriteLine($"Баланс: {accBalance:C}"); // :C - формат валюты
+            Console.WriteLine($"Тип счета: {accType}");
+            Console.WriteLine($"Числовой код типа: {(int)accType}");
+        }
+
+    }
+
+}
---
+++ BankAccountWithStructure/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountWithStructure
+{
+    internal enum AccountType
+    {
+        Checking,
+        Deposit,
+        Savings,
+        Credit,
+        Investment,
+        FixedTermDeposit,
+        SavingsAccount,
+        PensionDeposit,
+        StudentDeposit
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8; 
+            CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
+            CultureInfo.CurrentUICulture = new CultureInfo("ru-RU");
+            Console.WriteLine("Упражнение: Структура для банковских счетов");
+            Console.WriteLine("===========================================\n");
+     
+            Console.WriteLine("\nНажмите любую клавишу для завершения...");
+            Console.ReadKey();
+
+             BankAccount myAccount = new BankAccount(
+                 817810500012345678L,
+                 150000.75m,
+                 AccountType.Deposit
+             );
+         myAccount.DisplayAccountInfo();
+
+
+
+        }
+    }
+}
