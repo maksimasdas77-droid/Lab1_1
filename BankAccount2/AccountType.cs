f91f47add294a7ebcc602fe7ddaaad71a09449c1
Максим Дмитрачёнок <maksimasdas77@gmail.com>
2026-01-29 21:59:18 +0300

    Обновление информации (дальше делаю лабу)

---
+++ BankAccount2/AccountType.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount2
+{
+    internal enum AccountType
+    {
+        Checking,
+        Deposit,
+        Savings,
+        Credit,
+        Investment,
+        FixedTermDeposit,
+        SavingsAccount,
+        PensionDeposit,
+        StudentDeposit
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.Clear();
+                Console.WriteLine("\n=== Выбор типа депозита ===");
+                Console.WriteLine("Доступные типы счетов:");
+                int index = 1;
+                foreach (AccountType type in Enum.GetValues(typeof(AccountType)))
+                {
+                    Console.WriteLine($"{index}. {type}");
+                    index++;
+                }
+                Console.WriteLine("0. Выход"); 
+                Console.Write("\nВыберите номер типа счета для нового депозита: \n");
+                try
+                {
+
+                    int choise = int.Parse(Console.ReadLine());
+                    if (choise == 0) 
+                    {
+                        Console.WriteLine("\nВыход из программы...");
+                        break; 
+                    }
+                    if (choise >= 1 && choise <= Enum.GetValues(typeof(AccountType)).Length)
+                    {
+                        AccountType selectedType = (AccountType)(choise - 1);
+                        Console.WriteLine($"\n Вы выбрали: {selectedType}");
+                        Console.WriteLine($"Числовое значение: {(int)selectedType}");
+                        switch (selectedType)
+                        {
+                            case AccountType.Checking:
+                                Console.WriteLine("Описание: " +
+                                    "Текущий счет для ежедневных операций");
+                                break;
+                            case AccountType.Deposit: Console.WriteLine("Описание: " +
+                        "Срочный вклад с фиксированной ставкой");
+                                break;
+                            case AccountType.Savings:
+                                Console.WriteLine("Описание: " +
+                        "Накопительный счет с процентами");
+                                break;
+                            case AccountType.Credit:
+                                Console.WriteLine("Описание: " +
+                        "Кредитный счет с установленным лимитом");
+                                break;
+                            case AccountType.Investment:
+                                Console.WriteLine("Описание: " +
+                                    "Инвестиционный счет для ценных бумаг");
+                                break;
+                            case AccountType.FixedTermDeposit:
+                                Console.WriteLine("Описание: " + "Срочный депозит");
+                                break;
+                            case AccountType.SavingsAccount:
+                                Console.WriteLine("Описание: " + "накопительный счет");
+                                break;
+                            case AccountType.PensionDeposit:
+                                Console.WriteLine("Описание: " + "Пенсионный депозит");
+                                break;
+                            case AccountType.StudentDeposit:
+                                Console.WriteLine("Описание: Студенческий депозит");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: выбран неверный номер!");
+                    }
+                }
+            catch(FormatException)
+            {
+                    Console.WriteLine("Ошибка: введите корректное число!");
+                }
+                Console.WriteLine("\nНажмите любую клавишу для закрытия...");
+                Console.ReadKey();
+
+            }
+        }
+    } 
+    
+}
+
+
--- Lab2_1/Enum.cs
+++ Lab2_1/Enum.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Lab2_1
 {
+   enum AccountType
+    {
+        checking,
+        deposit,
+        savings,
+        credit
+    }
     enum DayWeek
     {
         Monday,
@@ -36,29 +43,41 @@ namespace Lab2_1
        
         static void Main(string[] args)
         {
-            while (true) {
-                Console.Clear();
-                Console.WriteLine("Введи число месяца (или 0 для выхода):");
-            string input = Console.ReadLine();
-                int i;
-                if (!int.TryParse(input, out i))
-                {
-                    Console.WriteLine("Ошибка: нужно ввести число!"); continue; // возвращаемся в начало цикла }
-                }
-                if (i == 0) { Console.WriteLine("Выход из программы..."); break; }
-                i = i - 1;
-                if (i >= 0 && i < 12)
-                {
-                    Console.WriteLine((Monts)i);
-                }
-                else
-                {
-                    Console.WriteLine("Нет такого месяца у тебя, либо добавь, либо напиши другой вариант");
+            //while (true) {
+            //    Console.Clear();
+            //    Console.WriteLine("Введи число месяца (или 0 для выхода):");
+            //string input = Console.ReadLine();
+            //    int i;
+            //    if (!int.TryParse(input, out i))
+            //    {
+            //        Console.WriteLine("Ошибка: нужно ввести число!"); continue; // возвращаемся в начало цикла }
+            //    }
+            //    if (i == 0) { Console.WriteLine("Выход из программы..."); break; }
+            //    i = i - 1;
+            //    if (i >= 0 && i < 12)
+            //    {
+            //        Console.WriteLine((Monts)i);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Нет такого месяца у тебя, либо добавь, либо напиши другой вариант");
 
-                }
-                Console.WriteLine("Нажми любую клавишу..."); 
-                Console.ReadKey();
-            }                 
+            //    }
+            //    Console.WriteLine("Нажми любую клавишу..."); 
+            //    Console.ReadKey();
+            //}
+            AccountType account1 = AccountType.checking;
+            AccountType account2 = AccountType.deposit;
+
+            Console.WriteLine("Тип первого счета: " + account1);
+            Console.WriteLine($"Тип второго счета: {account2}");
+
+            Console.WriteLine("\n Дополнительная информация:");
+            Console.WriteLine("Первый счет (как строка): " + account1.ToString());
+            Console.WriteLine("Второй счет (числовое значение): " + (int)account2);
+
+     
+
         }
     }
 }
